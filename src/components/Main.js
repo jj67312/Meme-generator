1a73f7f8c36b57d098a1b577fc6b1d93c38c618f
Jay Jani <jayjani002@gmail.com>
2022-09-09 23:46:01 +0530

    Added image generation using state and useState method.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,33 @@
 import React from 'react';
 
-import img from '../images/test.jpg';
 import memesData from '../memesData';
 
 export function Main() {
+  const [memeImage, setMemeImage] = React.useState('');
+
   function getImage() {
     // randomly pick up an image from the memesData file
     const memeData = memesData.data.memes;
-    const randomIndex = Math.floor(Math.random() * memeData.length);
-    console.log(memeData[randomIndex].url);
+    const randomIndex = Math.floor(Math.random() * memeData.length) + 1;
+    setMemeImage(memeData[randomIndex].url);
   }
+
+  const isGoingOut = false;
+  let answer = isGoingOut ? 'Yes' : 'No';
 
   return (
     <main>
       <div className='main-body'>
-        <form action='#'>
-          <div className='main-input'>
-            <input type='text' placeholder='Top text' autoFocus='autofocus' />
-            <input type='text' placeholder='Bottom text' />
-          </div>
-          <button className='btn' onClick={getImage}>
-            Get a new meme image
-          </button>
-        </form>
+        <div className='main-input'>
+          <input type='text' placeholder='Top text' autoFocus='autofocus' />
+          <input type='text' placeholder='Bottom text' />
+        </div>
+        <button className='btn' onClick={getImage}>
+          Get a new meme image
+        </button>
       </div>
       <div className='main-img'>
-        <img src={img} alt=''></img>
+        <img src={memeImage} alt=''></img>
       </div>
     </main>
   );
