e8a607df3749c3452643da7fb993cd3c373d0106
Jay Jani <jayjani002@gmail.com>
2022-09-11 12:27:01 +0530

    Updates

---
+++ precache-manifest.15696c5007aba66e2b1d51d56b337abe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac5c4966e301316d9fbf",
+    "url": "/Meme-generator/static/css/main.c2401281.chunk.css"
+  },
+  {
+    "revision": "ac5c4966e301316d9fbf",
+    "url": "/Meme-generator/static/js/main.ac5c4966.chunk.js"
+  },
+  {
+    "revision": "07c9f322fb10ec45539d",
+    "url": "/Meme-generator/static/js/1.07c9f322.chunk.js"
+  },
+  {
+    "revision": "c22f0935c59665a555a4",
+    "url": "/Meme-generator/static/js/runtime~main.c22f0935.js"
+  },
+  {
+    "revision": "1fcb5ddacea86bf9e9aaf4e257623baf",
+    "url": "/Meme-generator/index.html"
+  }
+];
